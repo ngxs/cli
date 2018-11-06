102b1e077ab93ecb45219eb744aff6776a468b49
Eran Shmil <eranshmil@users.noreply.github.com>
2018-11-06 15:28:10 +0200

    feat(build): add automatic reformat and commit lint

--- index.ts
+++ index.ts
@@ -3,15 +3,15 @@ import { OPTIONS_DEFINITIONS } from './src/command-line-parser/command-line.conf
 import { ArgvTable } from './src/command-line-parser/command-line.interface';
 import { NgxsCliExecutor } from './src/cli-executor/cli-executor.class';
 
-CommandLine
-    .parse(OPTIONS_DEFINITIONS)
-    .then((argv: ArgvTable) => {
-        if (argv.help) {
-            CommandLine.help();
-        } else {
-            NgxsCliExecutor.run(argv);
-        }
-    }).catch((e: Error) => {
+CommandLine.parse(OPTIONS_DEFINITIONS)
+  .then((argv: ArgvTable) => {
+    if (argv.help) {
+      CommandLine.help();
+    } else {
+      NgxsCliExecutor.run(argv);
+    }
+  })
+  .catch((e: Error) => {
     console.error(e.message);
     CommandLine.help();
-});
+  });
--- src/command-line-parser/command-line.class.ts
+++ src/command-line-parser/command-line.class.ts
@@ -5,44 +5,42 @@ import { HELP_SECTIONS, PARSE_OPTIONS } from './command-line.config';
 import { red } from 'colors/safe';
 
 export class CommandLine {
+  private static argv;
 
-    private static argv;
+  public static async parse(definitions: CommandLineDefinitions): Promise<ArgvTable> {
+    const argv = commandLineArgs(definitions, PARSE_OPTIONS);
+    this.argv = this.validate(argv);
+    return this.argv;
+  }
 
-    public static async parse(definitions: CommandLineDefinitions): Promise<ArgvTable> {
-        const argv = commandLineArgs(definitions, PARSE_OPTIONS);
-        this.argv = this.validate(argv);
-        return this.argv;
+  public static help(): void {
+    console.log(commandLineUsage(HELP_SECTIONS));
+    process.exit();
+  }
+
+  public static setArgument(key: string, value: string): void {
+    const argv = [];
+    let withoutIndex = null;
+
+    process.argv.forEach((val, index) => {
+      if (val === key) {
+        withoutIndex = index + 1;
+      } else if (index !== withoutIndex) {
+        argv.push(val);
+      }
+    });
+
+    argv.push(key, value);
+    process.argv = argv;
+  }
+
+  private static validate(argv): void {
+    const UNKNOWN_COMMAND = argv['_unknown'];
+    if (UNKNOWN_COMMAND) {
+      const errorMessage = `${red('[ERROR]:')} UNKNOWN_COMMAND ${JSON.stringify(UNKNOWN_COMMAND)}`;
+      throw new Error(errorMessage);
     }
 
-    public static help(): void {
-        console.log(commandLineUsage(HELP_SECTIONS));
-        process.exit();
-    }
-
-    public static setArgument(key: string, value: string): void {
-        const argv = [];
-        let withoutIndex = null;
-
-        process.argv.forEach((val, index) => {
-            if (val === key) {
-                withoutIndex = index + 1;
-            } else if (index !== withoutIndex) {
-                argv.push(val);
-            }
-        });
-
-        argv.push(key, value);
-        process.argv = argv;
-    }
-
-    private static validate(argv): void {
-        const UNKNOWN_COMMAND = argv['_unknown'];
-        if (UNKNOWN_COMMAND) {
-            const errorMessage = `${red('[ERROR]:')} UNKNOWN_COMMAND ${JSON.stringify(UNKNOWN_COMMAND)}`;
-            throw new Error(errorMessage);
-        }
-
-        return argv;
-    }
-
+    return argv;
+  }
 }
